9d69963892255d3da8c6de4658a0af8505d7585c
Paweł Walus <pawel.walus@protonmail.com>
2023-03-29 12:41:04 +0200

    benchmarks

--- benches/main.rs
+++ benches/main.rs
@@ -4,22 +4,23 @@ mod bench_array_create;
 mod bench_array_sum;
 mod bench_array_product;
 
-criterion_group!(benches,
-    // =====
+criterion_group!(array_create,
     bench_array_create::create_array_1d_1m_benchmark,
     bench_array_create::create_array_1d_10m_benchmark,
     bench_array_create::create_array_5d_1m_benchmark,
     bench_array_create::create_array_5d_10m_benchmark,
     bench_array_create::create_array_10d_1m_benchmark,
     bench_array_create::create_array_10d_10m_benchmark,
-    // =====
+);
+criterion_group!(array_sum,
     bench_array_sum::array_sum_1d_1m_benchmark,
     bench_array_sum::array_sum_1d_10m_benchmark,
     bench_array_sum::array_sum_5d_1m_benchmark,
     bench_array_sum::array_sum_5d_10m_benchmark,
     bench_array_sum::array_sum_10d_1m_benchmark,
     bench_array_sum::array_sum_10d_10m_benchmark,
-    // =====
+);
+criterion_group!(array_product,
     bench_array_product::array_product_1d_1m_benchmark,
     bench_array_product::array_product_1d_10m_benchmark,
     bench_array_product::array_product_5d_1m_benchmark,
@@ -27,4 +28,4 @@ criterion_group!(benches,
     bench_array_product::array_product_10d_1m_benchmark,
     bench_array_product::array_product_10d_10m_benchmark,
 );
-criterion_main!(benches);
+criterion_main!(array_create, array_sum, array_product);
