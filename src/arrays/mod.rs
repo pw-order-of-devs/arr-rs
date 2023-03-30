492e2cf5c1963debe23bd0ec26cd063c8bca70bd
Paweł Walus <123729832+pw-order-of-devs@users.noreply.github.com>
2023-03-30 09:56:00 +0200

    implement PartialEq and PartialCmp for Arrays (#39)

--- src/arrays/mod.rs
+++ src/arrays/mod.rs
@@ -1,6 +1,9 @@
 /// default implementation of array
 pub mod array;
 
+/// array compare traits
+pub mod compare;
+
 /// create array macro implementation
 #[cfg(feature = "macros")]
 pub mod macros;
