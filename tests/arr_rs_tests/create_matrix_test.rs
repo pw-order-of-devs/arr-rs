c0b7a2d731a79791295f8715137aa1b90076d285
Paweł Walus <pawel.walus@protonmail.com>
2023-04-06 16:18:20 +0200

    extend array create - tri, tril, triu

--- tests/arr_rs_tests/create_matrix_test.rs
+++ tests/arr_rs_tests/create_matrix_test.rs
@@ -28,3 +28,41 @@ case(array![[1, 2], [3, 4]], Some(-1), array!([[0, 0, 0, 0, 0], [1, 0, 0, 0, 0],
 )] fn test_diagflat(array: Array<i32>, k: Option<isize>, expected: Array<i32>) {
     assert_eq!(expected, Array::diagflat(&array, k))
 }
+
+#[rstest(
+n, m, k, expected,
+case(2, Some(2), None, array!([[1, 0], [1, 1]])),
+case(3, Some(3), None, array!([[1, 0, 0], [1, 1, 0], [1, 1, 1]])),
+case(3, Some(3), Some(0), array!([[1, 0, 0], [1, 1, 0], [1, 1, 1]])),
+case(3, Some(3), Some(1), array!([[1, 1, 0], [1, 1, 1], [1, 1, 1]])),
+case(3, Some(3), Some(-1), array!([[0, 0, 0], [1, 0, 0], [1, 1, 0]])),
+case(2, Some(4), Some(0), array!([[1, 0, 0, 0], [1, 1, 0, 0]])),
+case(2, Some(4), Some(1), array!([[1, 1, 0, 0], [1, 1, 1, 0]])),
+case(2, Some(4), Some(-1), array!([[0, 0, 0, 0], [1, 0, 0, 0]])),
+)] fn test_tri(n: usize, m: Option<usize>, k: Option<isize>, expected: Array<i32>) {
+    assert_eq!(expected, Array::tri(n, m, k))
+}
+
+#[rstest(
+array, k, expected,
+case(array_arange!(1, 9).reshape(vec![2, 4]), Some(0), array!([[1, 0, 0, 0], [5, 6, 0, 0]])),
+case(array_arange!(1, 9).reshape(vec![2, 4]), Some(1), array!([[1, 2, 0, 0], [5, 6, 7, 0]])),
+case(array_arange!(1, 9).reshape(vec![2, 4]), Some(-1), array!([[0, 0, 0, 0], [5, 0, 0, 0]])),
+case(array_arange!(1, 9).reshape(vec![2, 2, 2]), Some(0), array!([[[1, 0], [3, 4]], [[5, 0], [7, 8]]])),
+case(array_arange!(1, 9).reshape(vec![2, 2, 2]), Some(1), array!([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])),
+case(array_arange!(1, 9).reshape(vec![2, 2, 2]), Some(-1), array!([[[0, 0], [3, 0]], [[0, 0], [7, 0]]])),
+)] fn test_tril(array: Array<i32>, k: Option<isize>, expected: Array<i32>) {
+    assert_eq!(expected, array.tril(k))
+}
+
+#[rstest(
+array, k, expected,
+case(array_arange!(1, 9).reshape(vec![2, 4]), Some(0), array!([[1, 2, 3, 4], [0, 6, 7, 8]])),
+case(array_arange!(1, 9).reshape(vec![2, 4]), Some(1), array!([[0, 2, 3, 4], [0, 0, 7, 8]])),
+case(array_arange!(1, 9).reshape(vec![2, 4]), Some(-1), array!([[1, 2, 3, 4], [5, 6, 7, 8]])),
+case(array_arange!(1, 9).reshape(vec![2, 2, 2]), Some(0), array!([[[1, 2], [0, 4]], [[5, 6], [0, 8]]])),
+case(array_arange!(1, 9).reshape(vec![2, 2, 2]), Some(1), array!([[[0, 2], [0, 0]], [[0, 6], [0, 0]]])),
+case(array_arange!(1, 9).reshape(vec![2, 2, 2]), Some(-1), array!([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])),
+)] fn test_triu(array: Array<i32>, k: Option<isize>, expected: Array<i32>) {
+    assert_eq!(expected, array.triu(k))
+}
