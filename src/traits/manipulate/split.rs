0d0f5492412f6b85c3d6206d8234627b898824cc
Paweł Walus <pawel.walus@protonmail.com>
2023-04-21 17:18:02 +0200

    split implementation

--- src/traits/manipulate/split.rs
+++ src/traits/manipulate/split.rs
@@ -23,4 +23,25 @@ pub trait ArraySplit<N: Numeric> where Self: Sized + Clone {
     /// assert_eq!(vec![array!(0, 1, 2), array!(3, 4), array!(5, 6), array!(7, 8)], split);
     /// ```
     fn array_split(&self, parts: usize, axis: Option<usize>) -> Vec<Self>;
+
+    /// Split an array into multiple sub-arrays of equal size
+    ///
+    /// # Arguments
+    ///
+    /// * `parts` - indices defining how to split the array
+    /// * `axis` - the axis along which to split. optional, defaults to 0
+    ///
+    /// # Examples
+    /// ```
+    /// use arr_rs::prelude::*;
+    ///
+    /// let arr = array_arange!(0, 8);
+    /// let split = arr.split(3, None);
+    /// assert_eq!(vec![array!(0, 1, 2), array!(3, 4, 5), array!(6, 7, 8)], split);
+    ///
+    /// let arr = array_arange!(0, 7);
+    /// let split = arr.array_split(4, None);
+    /// assert_eq!(vec![array!(0, 1), array!(2, 3), array!(4, 5), array!(6, 7)], split);
+    /// ```
+    fn split(&self, parts: usize, axis: Option<usize>) -> Vec<Self>;
 }
--- tests/arr_rs_tests/split_test.rs
+++ tests/arr_rs_tests/split_test.rs
@@ -18,3 +18,22 @@ case(array_arange!(0, 7), 3, Some(2), vec![]),
 )] fn test_array_split(array: Array<i32>, parts: usize, axis: Option<usize>, expected: Vec<Array<i32>>) {
     assert_eq!(expected, array.array_split(parts, axis))
 }
+
+#[rstest(
+array, parts, axis, expected,
+case(Array::empty(), 1, None, vec![Array::empty()]),
+case(array!(1), 1, None, vec![array!(1)]),
+case(array_arange!(0, 7), 1, None, vec![array_arange!(0, 7)]),
+case(array_arange!(0, 8), 3, None, vec![array![0, 1, 2], array![3, 4, 5], array![6, 7, 8]]),
+case(array_arange!(0, 7).reshape(vec![4, 2]), 2, Some(0), vec![array!([[0, 1], [2, 3]]), array!([[4, 5], [6, 7]])]),
+case(array_arange!(0, 8).reshape(vec![3, 3]), 3, Some(1), vec![array!([[0], [3], [6]]), array!([[1], [4], [7]]), array!([[2], [5], [8]])]),
+case(array_arange!(0, 7).reshape(vec![2, 2, 2]), 2, None, vec![array!([[[0, 1], [2, 3]]]), array!([[[2, 3], [4, 5]]])]),
+#[should_panic(expected = "array split does not result in an equal division")]
+case(array_arange!(0, 7), 3, None, vec![array![0, 1, 2], array![3, 4, 5], array![6, 7]]),
+#[should_panic(expected = "number of sections must be larger than 0")]
+case(array_arange!(0, 7), 0, None, vec![]),
+#[should_panic(expected = "axis out of bounds")]
+case(array_arange!(0, 7), 3, Some(2), vec![]),
+)] fn test_split(array: Array<i32>, parts: usize, axis: Option<usize>, expected: Vec<Array<i32>>) {
+    assert_eq!(expected, array.split(parts, axis))
+}
