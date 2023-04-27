8c922bf753b19fb4e68405692c9b11471164583a
Paweł Walus <pawel.walus@protonmail.com>
2023-04-27 11:30:55 +0200

    vstack, hstack, dstack implementation

--- src/arrays/manipulate/stack.rs
+++ src/arrays/manipulate/stack.rs
@@ -14,14 +14,7 @@ impl <N: Numeric> ArrayStack<N> for Array<N> {
 
     fn concatenate(arrs: Vec<Self>, axis: Option<usize>) -> Self {
         if arrs.is_empty() { return Self::empty() }
-        if let Some(axis) = axis {
-            arrs.iter().for_each(|arr| assert!(axis < arr.ndim(), "axis out of bounds"));
-            (0 .. arrs.len() - 1).for_each(|i| {
-                let shape_1 = arrs[i].get_shape().remove_at(axis);
-                let shape_2 = arrs[i + 1].get_shape().remove_at(axis);
-                assert_eq!(shape_1, shape_2, "incompatible shapes for concatenate")
-            });
-        }
+        if let Some(axis) = axis { Self::validate_stack_shapes(&arrs, axis, axis) }
 
         let (mut arrs, initial) = (arrs.clone(), arrs[0].clone());
         arrs.remove_at(0).into_iter()
@@ -40,5 +33,55 @@ impl <N: Numeric> ArrayStack<N> for Array<N> {
         arrs.remove_at(0).into_iter()
             .fold(initial, |a, b| a.append(&b, Some(axis)))
             .reshape(new_shape)
+    }
+
+    fn vstack(arrs: Vec<Self>) -> Self {
+        if arrs.is_empty() { Self::empty() }
+        else {
+            Self::validate_stack_shapes(&arrs, 0, 0);
+            let mut new_shape = arrs[0].get_shape();
+            if new_shape.len() == 1 { new_shape.insert_at(0, arrs.len()); }
+            else { new_shape[0] = arrs.iter().fold(0, |a, b| a + b.shape[0]); }
+
+            Self::concatenate(arrs, Some(0)).reshape(new_shape)
+        }
+    }
+
+    fn hstack(arrs: Vec<Self>) -> Self {
+        if arrs.is_empty() { Self::empty() }
+        else if arrs.iter().all(|arr| arr.ndim() == 1) {
+            Self::concatenate(arrs, Some(0))
+        } else {
+            let arrs = arrs.iter().map(|arr| arr.atleast(2)).collect::<Vec<Self>>();
+            Self::validate_stack_shapes(&arrs, 1, 0);
+            let mut new_shape = arrs[0].get_shape();
+            new_shape[1] = arrs.iter().fold(0, |a, b| a + b.shape[1]);
+
+            Self::concatenate(arrs, Some(1)).reshape(new_shape)
+        }
+    }
+
+    fn dstack(arrs: Vec<Self>) -> Self {
+        if arrs.is_empty() { return Self::empty() }
+        else {
+            let arrs = arrs.iter().map(|arr| arr.atleast(3)).collect::<Vec<Self>>();
+            Self::validate_stack_shapes(&arrs, 2, 0);
+            let mut new_shape = arrs[0].get_shape();
+            new_shape[2] = arrs.iter().fold(0, |a, b| a + b.shape[2]);
+
+            Self::concatenate(arrs, Some(2)).reshape(new_shape)
+        }
+    }
+}
+
+impl <N: Numeric> Array<N> {
+
+    fn validate_stack_shapes(arrs: &Vec<Self>, axis: usize, remove_at: usize) {
+        arrs.iter().for_each(|arr| assert!(axis < arr.ndim(), "axis out of bounds"));
+        (0 .. arrs.len() - 1).for_each(|i| {
+            let shape_1 = arrs[i].get_shape().remove_at(remove_at);
+            let shape_2 = arrs[i + 1].get_shape().remove_at(remove_at);
+            assert_eq!(shape_1, shape_2, "incompatible shapes for concatenate")
+        });
     }
 }
--- tests/arr_rs_tests/stack_test.rs
+++ tests/arr_rs_tests/stack_test.rs
@@ -34,3 +34,41 @@ case(vec![array!([[1, 2], [3, 4]]), array!([[5, 6]])], None, array!([[[1, 2], [3
 )] fn test_stack(arrs: Vec<Array<i32>>, axis: Option<usize>, expected: Array<i32>) {
     assert_eq!(expected, Array::stack(arrs, axis))
 }
+
+#[rstest(
+arrs, expected,
+case(vec![array!([1, 2, 3]), array!([4, 5, 6])], array!([[1, 2, 3], [4, 5, 6]])),
+case(vec![array!([[1, 2], [3, 4]]), array!([[5, 6], [7, 8], [9, 10]])], array!([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])),
+case(vec![array!([[1]]), array!([[2]]), array!([[3]])], array!([[1], [2], [3]])),
+#[should_panic(expected = "incompatible shapes for concatenate")]
+case(vec![array!([[1, 2, 3], [1, 2, 3]]), Array::empty()], Array::empty()),
+#[should_panic(expected = "incompatible shapes for concatenate")]
+case(vec![array!([[1, 2, 3], [1, 2, 3]]), array!([[5, 6]])], Array::empty()),
+)] fn test_vstack(arrs: Vec<Array<i32>>, expected: Array<i32>) {
+    assert_eq!(expected, Array::vstack(arrs))
+}
+
+#[rstest(
+arrs, expected,
+case(vec![array!([1, 2, 3]), array!([4, 5, 6])], array!([1, 2, 3, 4, 5, 6])),
+case(vec![array!([1, 2, 3]), array!([4, 5, 6]), array!([7, 8])], array!([1, 2, 3, 4, 5, 6, 7, 8])),
+case(vec![array!([[1, 2], [3, 4]]), array!([[5, 6], [7, 8]])], array!([[1, 2, 5, 6], [3, 4, 7, 8]])),
+case(vec![array!([[1]]), array!([[2]]), array!([[3]])], array!([[1, 2, 3]])),
+#[should_panic(expected = "incompatible shapes for concatenate")]
+case(vec![array!([[1, 2], [1, 2]]), array!([5, 6])], Array::empty()),
+#[should_panic(expected = "incompatible shapes for concatenate")]
+case(vec![array!([[1, 2], [1, 2]]), array!([[5], [6]])], Array::empty()),
+)] fn test_hstack(arrs: Vec<Array<i32>>, expected: Array<i32>) {
+    assert_eq!(expected, Array::hstack(arrs))
+}
+
+#[rstest(
+arrs, expected,
+case(vec![array!([1, 2, 3]), array!([4, 5, 6])], array!([[[1, 4], [2, 5], [3, 6]]])),
+case(vec![array!([[1], [2], [3]]), array!([[4], [5], [6]])], array!([[[1, 4]], [[2, 5]], [[3, 6]]])),
+case(vec![array!([[1, 2], [3, 4]]), array!([[5, 6], [7, 8]])], array!([[[1, 5], [2, 6]], [[3, 7], [4, 8]]])),
+#[should_panic(expected = "incompatible shapes for concatenate")]
+case(vec![array!([1, 2, 3]), array!([4, 5, 6]), array!([7, 8])], Array::empty()),
+)] fn test_dstack(arrs: Vec<Array<i32>>, expected: Array<i32>) {
+    assert_eq!(expected, Array::dstack(arrs))
+}
