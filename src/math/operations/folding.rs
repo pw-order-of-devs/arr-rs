70f56e165e1731fdcc9d0bc39a4aa4886f2ac4cd
Paweł Walus <pawel.walus@protonmail.com>
2023-07-13 23:28:03 +0200

    math functions

--- src/math/operations/folding.rs
+++ src/math/operations/folding.rs
@@ -1,7 +1,9 @@
 use crate::{
     core::prelude::*,
     errors::prelude::*,
+    extensions::vec_ext::VecRemoveAt,
     numeric::prelude::*,
+    validators::prelude::*,
 };
 
 /// ArrayTrait - Array Folding functions
@@ -9,53 +11,73 @@ pub trait ArrayFolding<N: Numeric> where Self: Sized + Clone {
 
     /// Multiplication of array elements
     ///
+    /// # Arguments
+    ///
+    /// * `axis` - the axis along which to execute the function. optional. if negative, counts from last to first axis. if None, array is raveled
+    ///
     /// # Examples
     ///
     /// ```
     /// use arr_rs::prelude::*;
     ///
     /// let arr = Array::flat(vec![1, 2, 3, 4]);
-    /// assert_eq!(24, arr.prod().unwrap());
+    /// assert_eq!(Array::single(24), arr.prod(None));
     /// ```
-    fn prod(&self) -> Result<N, ArrayError>;
+    fn prod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError>;
 
     /// Sum of array elements
     ///
+    /// # Arguments
+    ///
+    /// * `axis` - the axis along which to execute the function. optional. if negative, counts from last to first axis. if None, array is raveled
+    ///
     /// # Examples
     ///
     /// ```
     /// use arr_rs::prelude::*;
     ///
     /// let arr = Array::flat(vec![1, 2, 3, 4]);
-    /// assert_eq!(10, arr.sum().unwrap());
+    /// assert_eq!(Array::single(10), arr.sum(None));
     /// ```
-    fn sum(&self) -> Result<N, ArrayError>;
+    fn sum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError>;
 
     /// Sum of array elements treating NaN as one
     ///
+    /// # Arguments
+    ///
+    /// * `axis` - the axis along which to execute the function. optional. if negative, counts from last to first axis. if None, array is raveled
+    ///
     /// # Examples
     ///
     /// ```
     /// use arr_rs::prelude::*;
     ///
-    /// let arr = Array::flat(vec![1., 2., 3., 4., f64::NAN]).unwrap();
-    /// assert_eq!(24., arr.nanprod().unwrap());
+    /// let arr = Array::flat(vec![1., 2., 3., 4., f64::NAN]);
+    /// assert_eq!(Array::single(24.), arr.nanprod(None));
     /// ```
-    fn nanprod(&self) -> Result<N, ArrayError>;
+    fn nanprod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError>;
 
     /// Sum of array elements treating NaN as zero
     ///
+    /// # Arguments
+    ///
+    /// * `axis` - the axis along which to execute the function. optional. if negative, counts from last to first axis. if None, array is raveled
+    ///
     /// # Examples
     ///
     /// ```
     /// use arr_rs::prelude::*;
     ///
-    /// let arr = Array::flat(vec![1., 2., 3., 4., f64::NAN]).unwrap();
-    /// assert_eq!(10., arr.nansum().unwrap());
+    /// let arr = Array::flat(vec![1., 2., 3., 4., f64::NAN]);
+    /// assert_eq!(Array::single(10.), arr.nansum(None));
     /// ```
-    fn nansum(&self) -> Result<N, ArrayError>;
+    fn nansum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError>;
 
     /// Cumulative product of array elements
+    ///
+    /// # Arguments
+    ///
+    /// * `axis` - the axis along which to execute the function. optional. if negative, counts from last to first axis. if None, array is raveled
     ///
     /// # Examples
     ///
@@ -63,11 +85,15 @@ pub trait ArrayFolding<N: Numeric> where Self: Sized + Clone {
     /// use arr_rs::prelude::*;
     ///
     /// let arr = Array::flat(vec![1, 2, 3, 4]).unwrap();
-    /// assert_eq!(Array::flat(vec![1, 2, 6, 24]), arr.cumprod());
+    /// assert_eq!(Array::flat(vec![1, 2, 6, 24]), arr.cumprod(None));
     /// ```
-    fn cumprod(&self) -> Result<Array<N>, ArrayError>;
+    fn cumprod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError>;
 
     /// Cumulative sum of array elements
+    ///
+    /// # Arguments
+    ///
+    /// * `axis` - the axis along which to execute the function. optional. if negative, counts from last to first axis. if None, array is raveled
     ///
     /// # Examples
     ///
@@ -75,126 +101,202 @@ pub trait ArrayFolding<N: Numeric> where Self: Sized + Clone {
     /// use arr_rs::prelude::*;
     ///
     /// let arr = Array::flat(vec![1, 2, 3, 4]).unwrap();
-    /// assert_eq!(Array::flat(vec![1, 3, 6, 10]), arr.cumsum());
+    /// assert_eq!(Array::flat(vec![1, 3, 6, 10]), arr.cumsum(None));
     /// ```
-    fn cumsum(&self) -> Result<Array<N>, ArrayError>;
+    fn cumsum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError>;
 
     /// Cumulative product of array elements
     ///
+    /// # Arguments
+    ///
+    /// * `axis` - the axis along which to execute the function. optional. if negative, counts from last to first axis. if None, array is raveled
+    ///
     /// # Examples
     ///
     /// ```
     /// use arr_rs::prelude::*;
     ///
     /// let arr = Array::flat(vec![1., 2., 3., 4., f64::NAN]).unwrap();
-    /// assert_eq!(Array::flat(vec![1., 2., 6., 24., 24.]), arr.nancumprod());
+    /// assert_eq!(Array::flat(vec![1., 2., 6., 24., 24.]), arr.nancumprod(None));
     /// ```
-    fn nancumprod(&self) -> Result<Array<N>, ArrayError>;
+    fn nancumprod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError>;
 
     /// Cumulative sum of array elements
     ///
+    /// # Arguments
+    ///
+    /// * `axis` - the axis along which to execute the function. optional. if negative, counts from last to first axis. if None, array is raveled
+    ///
     /// # Examples
     ///
     /// ```
     /// use arr_rs::prelude::*;
     ///
     /// let arr = Array::flat(vec![1., 2., 3., 4., f64::NAN]).unwrap();
-    /// assert_eq!(Array::flat(vec![1., 3., 6., 10., 10.]), arr.nancumsum());
+    /// assert_eq!(Array::flat(vec![1., 3., 6., 10., 10.]), arr.nancumsum(None));
     /// ```
-    fn nancumsum(&self) -> Result<Array<N>, ArrayError>;
+    fn nancumsum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError>;
 }
 
 impl <N: Numeric> ArrayFolding<N> for Array<N> {
 
-    fn prod(&self) -> Result<N, ArrayError> {
-        Ok(self.elements.iter().fold(N::one(), |acc, x| N::from(acc.to_f64() * x.to_f64())))
+    fn prod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        if let Some(axis) = axis {
+            self.folding_internal(axis, |arr| arr.prod(None))
+        } else {
+            Array::single(self.elements.iter().fold(N::one(), |acc, x| N::from(acc.to_f64() * x.to_f64())))
+        }
     }
 
-    fn sum(&self) -> Result<N, ArrayError> {
-        Ok(self.elements.iter().fold(N::zero(), |acc, x| N::from(acc.to_f64() + x.to_f64())))
+    fn sum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        if let Some(axis) = axis {
+            self.folding_internal(axis, |arr| arr.sum(None))
+        } else {
+            Array::single(self.elements.iter().fold(N::zero(), |acc, x| N::from(acc.to_f64() + x.to_f64())))
+        }
     }
 
-    fn nanprod(&self) -> Result<N, ArrayError> {
-        Ok(self.elements.iter().fold(N::one(), |acc, x| {
-            let x = if x.to_f64().is_nan() { 1. } else { x.to_f64() };
-            N::from(acc.to_f64() * x)
-        }))
+    fn nanprod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        if let Some(axis) = axis {
+            self.folding_internal(axis, |arr| arr.nanprod(None))
+        } else {
+            Array::single(self.elements.iter().fold(N::one(), |acc, x| {
+                let x = if x.to_f64().is_nan() { 1. } else { x.to_f64() };
+                N::from(acc.to_f64() * x)
+            }))
+        }
     }
 
-    fn nansum(&self) -> Result<N, ArrayError> {
-        Ok(self.elements.iter().fold(N::zero(), |acc, x| {
-            println!("{acc} {x}");
-            let x = if x.to_f64().is_nan() { 0. } else { x.to_f64() };
-            N::from(acc.to_f64() + x)
-        }))
+    fn nansum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        if let Some(axis) = axis {
+            self.folding_internal(axis, |arr| arr.nansum(None))
+        } else {
+            Array::single(self.elements.iter().fold(N::zero(), |acc, x| {
+                let x = if x.to_f64().is_nan() { 0. } else { x.to_f64() };
+                N::from(acc.to_f64() + x)
+            }))
+        }
     }
 
-    fn cumprod(&self) -> Result<Array<N>, ArrayError> {
-        let mut acc = N::one();
-        self.map(|&x| {
-            acc = N::from(acc.to_f64() * x.to_f64());
-            acc
-        })
+    fn cumprod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        if let Some(axis) = axis {
+            self.folding_internal_cum(axis, |arr| arr.cumprod(None))
+        } else {
+            let mut acc = N::one();
+            self.ravel()?.map(|&x| {
+                acc = N::from(acc.to_f64() * x.to_f64());
+                acc
+            })
+        }
     }
 
-    fn cumsum(&self) -> Result<Array<N>, ArrayError> {
-        let mut acc = N::zero();
-        self.map(|&x| {
-            acc = N::from(acc.to_f64() + x.to_f64());
-            acc
-        })
+    fn cumsum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        if let Some(axis) = axis {
+            self.folding_internal_cum(axis, |arr| arr.cumsum(None))
+        } else {
+            let mut acc = N::zero();
+            self.ravel()?.map(|&x| {
+                acc = N::from(acc.to_f64() + x.to_f64());
+                acc
+            })
+        }
     }
 
-    fn nancumprod(&self) -> Result<Array<N>, ArrayError> {
-        let mut acc = N::one();
-        self.map(|&x| {
-            let x = if x.to_f64().is_nan() { 1. } else { x.to_f64() };
-            acc = N::from(acc.to_f64() * x);
-            acc
-        })
+    fn nancumprod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        if let Some(axis) = axis {
+            self.folding_internal_cum(axis, |arr| arr.nancumprod(None))
+        } else {
+            let mut acc = N::one();
+            self.ravel()?.map(|&x| {
+                let x = if x.to_f64().is_nan() { 1. } else { x.to_f64() };
+                acc = N::from(acc.to_f64() * x);
+                acc
+            })
+        }
     }
 
-    fn nancumsum(&self) -> Result<Array<N>, ArrayError> {
-        let mut acc = N::zero();
-        self.map(|&x| {
-            let x = if x.to_f64().is_nan() { 0. } else { x.to_f64() };
-            acc = N::from(acc.to_f64() + x);
-            acc
-        })
+    fn nancumsum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        if let Some(axis) = axis {
+            self.folding_internal_cum(axis, |arr| arr.nancumsum(None))
+        } else {
+            let mut acc = N::zero();
+            self.ravel()?.map(|&x| {
+                let x = if x.to_f64().is_nan() { 0. } else { x.to_f64() };
+                acc = N::from(acc.to_f64() + x);
+                acc
+            })
+        }
     }
 }
 
 impl <N: Numeric> ArrayFolding<N> for Result<Array<N>, ArrayError> {
 
-    fn prod(&self) -> Result<N, ArrayError> {
-        self.clone()?.prod()
+    fn prod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        self.clone()?.prod(axis)
     }
 
-    fn sum(&self) -> Result<N, ArrayError> {
-        self.clone()?.sum()
+    fn sum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        self.clone()?.sum(axis)
     }
 
-    fn nanprod(&self) -> Result<N, ArrayError> {
-        self.clone()?.nanprod()
+    fn nanprod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        self.clone()?.nanprod(axis)
     }
 
-    fn nansum(&self) -> Result<N, ArrayError> {
-        self.clone()?.nansum()
+    fn nansum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        self.clone()?.nansum(axis)
     }
 
-    fn cumprod(&self) -> Result<Array<N>, ArrayError> {
-        self.clone()?.cumprod()
+    fn cumprod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        self.clone()?.cumprod(axis)
     }
 
-    fn cumsum(&self) -> Result<Array<N>, ArrayError> {
-        self.clone()?.cumsum()
+    fn cumsum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        self.clone()?.cumsum(axis)
     }
 
-    fn nancumprod(&self) -> Result<Array<N>, ArrayError> {
-        self.clone()?.nancumprod()
+    fn nancumprod(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        self.clone()?.nancumprod(axis)
     }
 
-    fn nancumsum(&self) -> Result<Array<N>, ArrayError> {
-        self.clone()?.nancumsum()
+    fn nancumsum(&self, axis: Option<isize>) -> Result<Array<N>, ArrayError> {
+        self.clone()?.nancumsum(axis)
+    }
+}
+
+impl <N: Numeric> Array<N> {
+
+    fn folding_internal<F: FnMut(&Array<N>) -> Result<Array<N>, ArrayError>>(&self, axis: isize, f: F) -> Result<Array<N>, ArrayError> {
+        let axis = Self::normalize_axis(axis, self.ndim()?);
+        let new_shape = self.get_shape()?.remove_at(axis);
+        let parts = self.get_shape()?.remove_at(axis).into_iter().product();
+        let partial = self.moveaxis(vec![axis as isize], vec![self.ndim()? as isize])?;
+        Array::folding_partial(&partial, parts, f)
+            .reshape(new_shape.clone())
+    }
+
+    fn folding_internal_cum<F>(&self, axis: isize, f: F) -> Result<Array<N>, ArrayError>
+        where F: FnMut(&Array<N>) -> Result<Array<N>, ArrayError> {
+        let axis = Self::normalize_axis(axis, self.ndim()?);
+        let parts = self.get_shape()?.remove_at(axis).into_iter().product();
+        let partial = self.moveaxis(vec![axis as isize], vec![self.ndim()? as isize])?;
+        let partial = Array::folding_partial(&partial, parts, f)
+            .reshape(partial.get_shape()?);
+        if axis == 0 { partial.rollaxis((self.ndim()? - 1) as isize, None) }
+        else { partial.moveaxis(vec![axis as isize], vec![self.ndim()? as isize]) }
+            .reshape(self.get_shape()?)
+    }
+
+    fn folding_partial<F>(partial: &Array<N>, parts: usize, mut f: F) -> Result<Array<N>, ArrayError>
+        where F: FnMut(&Array<N>) -> Result<Array<N>, ArrayError> {
+        let result = partial
+            .ravel()
+            .split(parts, None)?.into_iter()
+            .map(|arr| f(&arr))
+            .collect::<Vec<Result<Array<N>, _>>>()
+            .has_error()?.into_iter()
+            .flat_map(|arr| arr.unwrap())
+            .collect::<Array<N>>();
+        Ok(result)
     }
 }
--- tests/math/operations/folding_test.rs
+++ tests/math/operations/folding_test.rs
@@ -2,25 +2,122 @@ use rstest::rstest;
 use arr_rs::prelude::*;
 
 #[rstest(
-array, expected,
-case(array![1, 2, 3, 4], 24),
-case(array!([[1, 2], [3, 4]]), 24),
-// case(array![1, 2, 3, 4], None, array_flat!(24)),
-// case(array!([[1, 2], [3, 4]]), None, array_flat!(24)),
-// case(array!([[1, 2], [3, 4]]), Some(0), array_flat!(3, 8)),
-// case(array!([[1, 2], [3, 4]]), Some(1), array_flat!(2, 12)),
-// case(array_arange!(0, 23).reshape(vec![2, 3, 4]), None, array_flat!(0)),
-// case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(0), array!([[0, 13, 28, 45], [64, 85, 108, 133], [160, 189, 220, 253]])),
-// case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(1), array!([[0, 45, 120, 231], [3840, 4641, 5544, 6555]])),
-// case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(2), array!([[0, 840, 7920], [32760, 93024, 212520]])),
-)] fn test_prod(array: Result<Array<i32>, ArrayError>, expected: i32) {
-    assert_eq!(expected, array.prod().unwrap())
+array, axis, expected,
+case(array![1, 2, 3, 4], None, array_flat!(24)),
+case(array!([[1, 2], [3, 4]]), None, array_flat!(24)),
+case(array!([[1, 2], [3, 4]]), Some(0), array_flat!(3, 8)),
+case(array!([[1, 2], [3, 4]]), Some(1), array_flat!(2, 12)),
+case(array_arange!(0, 23).reshape(vec![2, 3, 4]), None, array_flat!(0)),
+case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(0), array!([[0, 13, 28, 45], [64, 85, 108, 133], [160, 189, 220, 253]])),
+case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(1), array!([[0, 45, 120, 231], [3840, 4641, 5544, 6555]])),
+case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(2), array!([[0, 840, 7920], [32760, 93024, 212520]])),
+)] fn test_prod(array: Result<Array<i32>, ArrayError>, axis: Option<isize>, expected: Result<Array<i32>, ArrayError>) {
+    assert_eq!(expected, array.prod(axis))
 }
 
 #[rstest(
-array, expected,
-case(array!([1, 2, 3, 4]), array!([1, 3, 6, 10])),
-case(array!([[1, 2], [3, 4]]), array!([[1, 3], [6, 10]])),
-)] fn test_cumsum(array: Result<Array<i32>, ArrayError>, expected: Result<Array<i32>, ArrayError>) {
-    assert_eq!(expected, array.cumsum())
+array, axis, expected,
+case(array![1, 2, 3, 4], None, array_flat!(10)),
+case(array!([[1, 2], [3, 4]]), None, array_flat!(10)),
+case(array!([[1, 2], [3, 4]]), Some(0), array_flat!(4, 6)),
+case(array!([[1, 2], [3, 4]]), Some(1), array_flat!(3, 7)),
+case(array_arange!(0, 23).reshape(vec![2, 3, 4]), None, array_flat!(276)),
+case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(0), array!([[12, 14, 16, 18], [20, 22, 24, 26], [28, 30, 32, 34]])),
+case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(1), array!([[12, 15, 18, 21], [48, 51, 54, 57]])),
+case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(2), array!([[6, 22, 38], [54, 70, 86]])),
+case(array_arange!(0, 23).reshape(vec![2, 3, 4]), Some(2), array!([[6, 22, 38], [54, 70, 86]])),
+)] fn test_sum(array: Result<Array<i32>, ArrayError>, axis: Option<isize>, expected: Result<Array<i32>, ArrayError>) {
+    assert_eq!(expected, array.sum(axis))
+}
+
+#[rstest(
+array, axis, expected,
+case(array![1., 2., 3., 4.], None, array_flat!(24.)),
+case(array!([[1., 2.], [3., 4.]]), None, array_flat!(24.)),
+case(array!([[1., f64::NAN], [3., f64::NAN]]), None, array_flat!(3.)),
+case(array!([[1., 2.], [3., 4.]]), Some(0), array_flat!(3., 8.)),
+case(array!([[1., 2.], [3., 4.]]), Some(1), array_flat!(2., 12.)),
+case(array!([[1., f64::NAN], [3., f64::NAN]]), Some(1), array_flat!(1., 3.)),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), None, array_flat!(0.)),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(0), array!([[0., 13., 28., 45.], [64., 85., 108., 133.], [160., 189., 220., 253.]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(1), array!([[0., 45., 120., 231.], [3840., 4641., 5544., 6555.]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(2), array!([[0., 840., 7920.], [32760., 93024., 212520.]])),
+)] fn test_nanprod(array: Result<Array<f64>, ArrayError>, axis: Option<isize>, expected: Result<Array<f64>, ArrayError>) {
+    assert_eq!(expected, array.nanprod(axis))
+}
+
+#[rstest(
+array, axis, expected,
+case(array![1., 2., 3., 4.], None, array_flat!(10.)),
+case(array!([[1., 2.], [3., 4.]]), None, array_flat!(10.)),
+case(array!([[1., f64::NAN], [3., f64::NAN]]), None, array_flat!(4.)),
+case(array!([[1., 2.], [3., 4.]]), Some(0), array_flat!(4., 6.)),
+case(array!([[1., 2.], [3., 4.]]), Some(1), array_flat!(3., 7.)),
+case(array!([[1., f64::NAN], [3., f64::NAN]]), Some(1), array_flat!(1., 3.)),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), None, array_flat!(276.)),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(0), array!([[12., 14., 16., 18.], [20., 22., 24., 26.], [28., 30., 32., 34.]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(1), array!([[12., 15., 18., 21.], [48., 51., 54., 57.]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(2), array!([[6., 22., 38.], [54., 70., 86.]])),
+)] fn test_nansum(array: Result<Array<f64>, ArrayError>, axis: Option<isize>, expected: Result<Array<f64>, ArrayError>) {
+    assert_eq!(expected, array.nansum(axis))
+}
+
+#[rstest(
+array, axis, expected,
+case(array![1., 2., 3., 4.], None, array_flat!(1., 2., 6., 24.)),
+case(array!([[1., 2.], [3., 4.]]), None, array_flat!(1., 2., 6., 24.)),
+case(array!([[1., 2.], [3., 4.]]), Some(0), array!([[1., 2.], [3., 8.]])),
+case(array!([[1., 2.], [3., 4.]]), Some(1), array!([[1., 2.], [3., 12.]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), None, array_full!(vec![24], 0.)),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(0), array!([[[0., 1., 2., 3.], [4., 5., 6., 7.], [8., 9., 10., 11.]], [[0., 13., 28., 45.], [64., 85., 108., 133.], [160., 189., 220., 253.]]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(1), array!([[[0., 1., 2., 3.], [0., 5., 12., 21.], [0., 45., 120., 231.]], [[12., 13., 14., 15.], [192., 221., 252., 285.], [3840., 4641., 5544., 6555.]]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(2), array!([[[0., 0., 0., 0.], [4., 20., 120., 840.], [8., 72., 720., 7920.]], [[12., 156., 2184., 32760.], [16., 272., 4896., 93024.], [20., 420., 9240., 212520.]]])),
+)] fn test_cumprod(array: Result<Array<f64>, ArrayError>, axis: Option<isize>, expected: Result<Array<f64>, ArrayError>) {
+    assert_eq!(expected, array.cumprod(axis))
+}
+
+#[rstest(
+array, axis, expected,
+case(array![1., 2., 3., 4.], None, array_flat!(1., 3., 6., 10.)),
+case(array!([[1., 2.], [3., 4.]]), None, array_flat!(1., 3., 6., 10.)),
+case(array!([[1., 2.], [3., 4.]]), Some(0), array!([[1., 2.], [4., 6.]])),
+case(array!([[1., 2.], [3., 4.]]), Some(1), array!([[1., 3.], [3., 7.]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), None, array_flat!(0., 1., 3., 6., 10., 15., 21., 28., 36., 45., 55., 66., 78., 91., 105., 120., 136., 153., 171., 190., 210., 231., 253., 276.)),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(0), array!([[[0., 1., 2., 3.], [4., 5., 6., 7.], [8., 9., 10., 11.]], [[12., 14., 16., 18.], [20., 22., 24., 26.], [28., 30., 32., 34.]]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(1), array!([[[0., 1., 2., 3.], [4., 6., 8., 10.], [12., 15., 18., 21.]], [[12., 13., 14., 15.], [28., 30., 32., 34.], [48., 51., 54., 57.]]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(2), array!([[[0., 1., 3., 6.], [4., 9., 15., 22.], [8., 17., 27., 38.]], [[12., 25., 39., 54.], [16., 33., 51., 70.], [20., 41., 63., 86.]]])),
+)] fn test_cumsum(array: Result<Array<f64>, ArrayError>, axis: Option<isize>, expected: Result<Array<f64>, ArrayError>) {
+    assert_eq!(expected, array.cumsum(axis))
+}
+
+#[rstest(
+array, axis, expected,
+case(array![1., 2., 3., 4.], None, array_flat!(1., 2., 6., 24.)),
+case(array!([[1., 2.], [3., 4.]]), None, array_flat!(1., 2., 6., 24.)),
+case(array!([[1., f64::NAN], [3., f64::NAN]]), None, array_flat!(1., 1., 3., 3.)),
+case(array!([[1., 2.], [3., 4.]]), Some(0), array!([[1., 2.], [3., 8.]])),
+case(array!([[1., 2.], [3., 4.]]), Some(1), array!([[1., 2.], [3., 12.]])),
+case(array!([[1., f64::NAN], [3., f64::NAN]]), Some(1), array!([[1., 1.], [3., 3.]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), None, array_full!(vec![24], 0.)),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(0), array!([[[0., 1., 2., 3.], [4., 5., 6., 7.], [8., 9., 10., 11.]], [[0., 13., 28., 45.], [64., 85., 108., 133.], [160., 189., 220., 253.]]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(1), array!([[[0., 1., 2., 3.], [0., 5., 12., 21.], [0., 45., 120., 231.]], [[12., 13., 14., 15.], [192., 221., 252., 285.], [3840., 4641., 5544., 6555.]]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(2), array!([[[0., 0., 0., 0.], [4., 20., 120., 840.], [8., 72., 720., 7920.]], [[12., 156., 2184., 32760.], [16., 272., 4896., 93024.], [20., 420., 9240., 212520.]]])),
+)] fn test_nancumprod(array: Result<Array<f64>, ArrayError>, axis: Option<isize>, expected: Result<Array<f64>, ArrayError>) {
+    assert_eq!(expected, array.nancumprod(axis))
+}
+
+#[rstest(
+array, axis, expected,
+case(array![1., 2., 3., 4.], None, array_flat!(1., 3., 6., 10.)),
+case(array!([[1., 2.], [3., 4.]]), None, array_flat!(1., 3., 6., 10.)),
+case(array!([[1., f64::NAN], [3., f64::NAN]]), None, array_flat!(1., 1., 4., 4.)),
+case(array!([[1., 2.], [3., 4.]]), Some(0), array!([[1., 2.], [4., 6.]])),
+case(array!([[1., 2.], [3., 4.]]), Some(1), array!([[1., 3.], [3., 7.]])),
+case(array!([[1., f64::NAN], [3., f64::NAN]]), Some(1), array!([[1., 1.], [3., 3.]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), None, array_flat!(0., 1., 3., 6., 10., 15., 21., 28., 36., 45., 55., 66., 78., 91., 105., 120., 136., 153., 171., 190., 210., 231., 253., 276.)),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(0), array!([[[0., 1., 2., 3.], [4., 5., 6., 7.], [8., 9., 10., 11.]], [[12., 14., 16., 18.], [20., 22., 24., 26.], [28., 30., 32., 34.]]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(1), array!([[[0., 1., 2., 3.], [4., 6., 8., 10.], [12., 15., 18., 21.]], [[12., 13., 14., 15.], [28., 30., 32., 34.], [48., 51., 54., 57.]]])),
+case(array_arange!(0., 23.).reshape(vec![2, 3, 4]), Some(2), array!([[[0., 1., 3., 6.], [4., 9., 15., 22.], [8., 17., 27., 38.]], [[12., 25., 39., 54.], [16., 33., 51., 70.], [20., 41., 63., 86.]]])),
+)] fn test_nancumsum(array: Result<Array<f64>, ArrayError>, axis: Option<isize>, expected: Result<Array<f64>, ArrayError>) {
+    assert_eq!(expected, array.nancumsum(axis))
 }
