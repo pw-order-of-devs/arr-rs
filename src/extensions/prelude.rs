41db6242a8c34fdca5a20c93fed7f016038a09ad
Paweł Walus <pawel.walus@protonmail.com>
2023-07-21 14:21:14 +0200

    display fixed

--- src/extensions/prelude.rs
+++ src/extensions/prelude.rs
@@ -1,5 +1,5 @@
 pub(crate) use crate::extensions::{
-    array_ext::ArrayExt,
+    array_ext::{ArrayExt, ArrayNumericExt},
     chars_ext::CharsJoin,
     iter_ext::IterSorted,
     vec_ext::{VecInsertAt, VecRemoveAt, VecUpdateAt, VecReverse},
